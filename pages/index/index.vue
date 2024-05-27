3ff6e4e26235adcf3640ad90dc5a08da5fe874e5
liuchao <liuchao02@nuit.edu.cn>
2024-05-27 17:00:01 +0800

    修改了Index.vue

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,6 +4,9 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
 	</view>
 </template>
 
