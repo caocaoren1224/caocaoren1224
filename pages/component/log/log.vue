7d8873de252a17518316eecd94cb3f11759c8f8f
liuchao <liuchao02@nuit.edu.cn>
2024-05-27 09:25:19 +0800

    大大大

---
+++ pages/component/log/log.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="uniUi-page-head">
+			<view class="uniUi-page-head-title">log</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+		onLoad() {
+			console.log("打印日志1");
+			console.info("打印日志2");
+			console.debug("打印日志3");
+			console.warn("打印日志4");
+			console.error("打印日志5");
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
